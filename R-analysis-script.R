3ac4688f52fa8b3247c0a63d50796b5d192d3854
Adam Dunstan <38237234+adam-dunstan@users.noreply.github.com>
2019-07-29 14:05:32 +1000

    Update R-analysis-script.R

--- R-analysis-script.R
+++ R-analysis-script.R
@@ -6,7 +6,7 @@ setwd(wd)
 my.files <- choose.files()
 my.data <- lapply(my.files, read.csv, header=TRUE, sep="\t", skip = "21", colClasses=c(NA,"NULL"))
 my.fingers <-  unlist(lapply(my.files, read.csv, header=FALSE, sep="\t", skip = "6", colClasses=c("NULL", NA), nrows = 1) )
-my.devices <-  unlist(sapply(my.files, read.csv, header=FALSE, sep="\t", simplify = TRUE, skip = "2", colClasses=c("NULL", NA), nrows = 1), recursive = TRUE)
+my.devices <-  factor(unlist(sapply(my.files, read.csv, header=FALSE, sep="\t", simplify = TRUE, skip = "2", colClasses=c("NULL", NA), nrows = 1), recursive = TRUE), ordered = TRUE)
 comment <- unlist(sapply(my.files, read.csv, header=FALSE, sep="\t", simplify = TRUE, skip = "10", colClasses=c("NULL", NA), nrows = 1), recursive = TRUE)
 testname <- unlist(sapply(my.files, read.csv, header=FALSE, sep="\t", simplify = TRUE, skip = "3", colClasses=c("NULL", NA), nrows = 1), recursive = TRUE)
 PCEvec <- unlist(lapply(my.files, read.csv, header=FALSE, sep="\t", skip = "13", colClasses=c("NULL", NA), nrows = 1) )
@@ -25,10 +25,10 @@ for (i in 1:length(my.files)){
   x <- paste("Finger", my.fingers[i],"Device",my.devices[i], sep =" " )
   
   plot(my.data[[i]]$U..V.,my.data[[i]]$J..mA.cm2., main = x, sub = paste("Fig.",i,":",comment), xlab = "Volatge (V)", ylab = "Jsc (mA/cm^2)", xlim = c(-0.5,1), ylim = c(-15,0)
-)
+  )
   axis(1, pos=0, labels = FALSE, tick = TRUE, col = "black")
   axis(2, pos=0, labels = FALSE, tick = TRUE, col = "black")
- dev.off()
+  dev.off()
 }
 #Next we will print a histogram of the PCEs of each finger as a generalization, then another catergorized by device. This may indicate issues with manufacturing methods & techniques.
 jpeg("Test-General-Boxplot.jpg")
@@ -36,15 +36,17 @@ boxplot(PCEvec, main = "Summary of Test Efficiency", ylab = "Power Conversion Ef
 dev.off()
 
 #Compare all devices using PCE. (To highlight differences between manufacturing different devices as they are produced one device at a time.)
-m1<-cbind(my.devices,PCEvec)
-m2<-split.data.frame(m1,my.devices, drop=FALSE)
-
+m1<-as.table(cbind(my.devices,PCEvec))
+m2 <-split.data.frame(m1, my.devices, drop = TRUE)
+m3 <-as.table(m2$OTH0001)
 #Compare all fingers using PCE. (To highlight differences on specific areas of the devices. May suggest particular areas that are often damaged or undeveloped.)
 
 #Tell user that the script is finished!
 library(beepr)
 beep(sound = 2)
-Sys.sleep(3)
+Sys.sleep(2)
 dev.off()
+Sys.sleep(2)
+print("Closing & saving script...")
 q()
 y
